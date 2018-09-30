864fd2a58630514657c6773a50d3729ead799d55
高鹏航 <penghang.gph@bdqkl-inc.com>
2018-09-30 15:56:43 +0800

    node端写json文件

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,7 @@
 const api = require('../controller/article')
 const Router = require('koa-router')
+const fs = require('fs')
+const path = require('path')
 const router = new Router({
   prefix: '/api'
 })
@@ -7,6 +9,36 @@ const router = new Router({
 module.exports = (app) => {
   router.get('/article', api.articleList)
   router.get('/article/:id', api.articleList)
+  router.put('/say', async (ctx, next) => {
+    console.log(ctx.request.body.say + '-----' + typeof ctx.request.body.say)
+    let json = {
+      'saySomething': ctx.request.body.say
+    }
+    const p = path.resolve('src/assets', 'say.json')
+    ctx.body = {
+      json,
+      p
+    }
+    try {
+      fs.writeFile(p, JSON.stringify(json), (err) => {
+        if (err) {
+          console.log(new Error(err))
+        }
+        console.log('文件已保存！')
+        ctx.body = {
+          success: true,
+          json
+        }
+      })
+    } catch (e) {
+      if (e) {
+        ctx.body = {
+          error: 'fail'
+        }
+        throw new Error(e)
+      }
+    }
+  })
   app.use(router.routes())
     .use(router.allowedMethods())
 }
